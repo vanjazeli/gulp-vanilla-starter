82895117ceeb77c94169d3af5d9f70e30ba39f72
vanjazeli <vanjazeli@gmail.com>
2023-05-28 10:46:47 +0200

    feat: split build into dev and prod

--- gulpfile.js
+++ gulpfile.js
@@ -21,8 +21,8 @@ gulp.task('watch', () => {
 			baseDir: './dist',
 		},
 	});
-	gulp.watch('./src/styles/**/*.scss').on('change', gulp.series('styles'));
-	gulp.watch('./src/js/**/*.js').on('change', gulp.series('javascript', browserSync.reload));
+	gulp.watch('./src/styles/**/*.scss').on('change', gulp.series('stylesDev'));
+	gulp.watch('./src/js/**/*.js').on('change', gulp.series('javascriptDev', browserSync.reload));
 	gulp.watch('./src/*.html').on('change', gulp.series('html', browserSync.reload));
 });
 
@@ -53,6 +53,10 @@ gulp.task('iconfont', () => {
 		.pipe(gulp.dest('./src/assets/fonts'));
 });
 
+gulp.task('fonts', () => {
+	return gulp.src('./src/assets/fonts/*').pipe(gulp.dest('./dist/assets/fonts'));
+});
+
 gulp.task('assets', () => {
 	return gulp
 		.src('./src/assets/**')
@@ -62,7 +66,7 @@ gulp.task('assets', () => {
 		});
 });
 
-gulp.task('javascript', () => {
+gulp.task('javascriptDev', () => {
 	return gulp
 		.src('./src/js/**/*.js')
 		.pipe(eslint())
@@ -71,6 +75,23 @@ gulp.task('javascript', () => {
 		.pipe(
 			webpack({
 				mode: 'development',
+				output: {
+					filename: 'main.js',
+				},
+			})
+		)
+		.pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('javascriptProd', () => {
+	return gulp
+		.src('./src/js/**/*.js')
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError())
+		.pipe(
+			webpack({
+				mode: 'production',
 				output: {
 					filename: 'main.js',
 				},
@@ -102,7 +123,19 @@ gulp.task('lint', () => {
 });
 
 gulp.task(
-	'styles',
+	'stylesDev',
+	gulp.series('lint', function stylePrep() {
+		return gulp
+			.src(['./src/styles/**/*.scss', '!./src/styles/iconfont/iconfont-template.scss'])
+			.pipe(sass())
+			.pipe(postcss([autoprefixer('last 2 versions')]))
+			.pipe(gulp.dest('./dist/styles'))
+			.pipe(browserSync.stream());
+	})
+);
+
+gulp.task(
+	'stylesProd',
 	gulp.series('lint', function stylePrep() {
 		return gulp
 			.src(['./src/styles/**/*.scss', '!./src/styles/iconfont/iconfont-template.scss'])
@@ -118,4 +151,6 @@ gulp.task('html', () => {
 	return gulp.src('src/*.{html,ico}').pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', gulp.series('javascript', 'html', 'iconfont', 'assets', 'styles', 'watch'));
+gulp.task('production', gulp.series('javascriptProd', 'html', 'iconfont', 'assets', 'stylesProd', 'watch'));
+
+gulp.task('development', gulp.series('javascriptDev', 'html', 'iconfont', 'assets', 'stylesDev', 'watch'));
