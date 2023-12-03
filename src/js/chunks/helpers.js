45f53062dbe086e43b6dc214c7827b7420f963fa
vanjazeli <vanjazeli@gmail.com>
2023-12-03 09:14:59 +0100

    feat: add comments and getPosition to helpers.js

--- src/js/chunks/helpers.js
+++ src/js/chunks/helpers.js
@@ -1,26 +1,39 @@
 const helpers = {
+	html: document.querySelector('html'),
+	scrollable: true,
+
+	// disables scroll
 	disableScroll: function () {
-		const html = document.querySelector('html');
-		html.style.overflow = 'hidden';
+		if (this.scrollable) {
+			this.html.style.overflow = 'hidden';
+			this.scrollable = false;
+		}
 	},
 
+	// enables scroll
 	enableScroll: function () {
-		const html = document.querySelector('html');
-		html.style.overflow = '';
-	},
-
-	strToBool: function (string) {
-		if (string === 'true') {
-			return true;
-		}
-		if (string === 'false') {
-			return false;
+		if (!this.scrollable) {
+			this.html.style.overflow = '';
+			this.scrollable = true;
 		}
 	},
 
+	// returns ture or false depending on whether an element is in viewport or not
 	isInView: function (element) {
 		const rect = element.getBoundingClientRect();
 		return rect.top <= (window.innerHeight || document.documentElement.clientHeight) && rect.bottom >= 0 && rect.left <= (window.innerWidth || document.documentElement.clientWidth) && rect.right >= 0;
+	},
+
+	// returns the position of an element on the page
+	getPosition: function (element) {
+		let xPosition = 0;
+		let yPosition = 0;
+		while (element) {
+			xPosition += element.offsetLeft - element.scrollLeft + element.clientLeft;
+			yPosition += element.offsetTop - element.scrollTop + element.clientTop;
+			element = element.offsetParent;
+		}
+		return { x: xPosition, y: yPosition };
 	},
 };
 
